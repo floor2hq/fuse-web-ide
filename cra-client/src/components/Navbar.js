e0c0d19b37af7ee07891e384b031cb4242e04134
= <=>
2023-10-07 17:05:04 +0530

    major flush hehe

---
+++ cra-client/src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div id="navbar_container">
+      <div id="nav_buttons">
+        <div id="logo"></div>
+
+        <button id="file_btn" className="button">
+          File
+        </button>
+        <button id="edit_btn" className="button">
+          Edit
+        </button>
+        <button id="selection_btn" className="button">
+          Selection
+        </button>
+        <button id="view_btn" className="button">
+          View
+        </button>
+        <button id="go_btn" className="button">
+          Go
+        </button>
+        <button id="run_btn" className="button">
+          Run
+        </button>
+      </div>
+      <input type="search" name="" id="search" placeholder="  Search" />
+      <div id="additional"></div>
+    </div>
+  );
+};
+
+export default Navbar;
