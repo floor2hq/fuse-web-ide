92146361675b9665078429c66195ae0d7accfd72
Soubhik <119437069+zakhaev26@users.noreply.github.com>
2023-10-07 17:49:49 +0530

    Update socket.js

--- cra-client/src/socket.js
+++ cra-client/src/socket.js
@@ -7,5 +7,5 @@ export const initSocket = async () => {
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io("http://localhost:5000", options);
+    return io("https://fuse-server-dmhy.onrender.com", options);
 };
